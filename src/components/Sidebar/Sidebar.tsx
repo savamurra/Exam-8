bd4269c65f9c020b4ce0145cd7fd9f68baa5d00a
Sanzhar Beishekeev <franki-55@mail.ru>
2024-10-26 17:32:43 +0600

    Создал компонент навигации по категориям и в home добавил условия для типа запросов что бы запрашивать цитаты выбранной категории

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+const categories = [
+  { name: 'All', path: '/' },
+  { name: 'Motivational', path: '/quotes/category/motivational' },
+  { name: 'Star Wars', path: '/quotes/category/star-wars' },
+  { name: 'Saying', path: '/quotes/category/saying' },
+  { name: 'Humour', path: '/quotes/category/humour' },
+  { name: 'Famous People', path: '/quotes/category/famous-people' },
+];
+
+
+
+const Sidebar: React.FC = () => {
+  return (
+    <Box sx={{display: 'grid'}}>
+      {categories.map((category) => (
+        <NavLink
+          key={category.name}
+          to={category.path}
+        >
+          {category.name}
+        </NavLink>
+      ))}
+    </Box>
+  );
+};
+
+export default Sidebar;
--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -3,27 +3,53 @@ import { IQuote, IQuoteAPI } from '../../types';
 import axiosAPI from '../../axiosAPI.tsx';
 import Grid from '@mui/material/Grid2';
 import QuoteItem from '../../components/QuoteItem/QuoteItem.tsx';
+import { useParams } from 'react-router-dom';
+import Sidebar from '../../components/Sidebar/Sidebar.tsx';
+import Spinner from '../../components/UI/Spinner/Spinner.tsx';
+
 
 const Home = () => {
-  const [quotes,setQuotes] = useState<IQuote[]>([]);
+  const [quotes, setQuotes] = useState<IQuote[]>([]);
+  const {category} = useParams<{ category: string }>();
+  const [loading, setLoading] = useState<boolean>(false);
 
   const fetchQuotes = useCallback(async () => {
     try {
-      const response : {data: IQuoteAPI} = await axiosAPI<IQuoteAPI>("quotes.json");
+      setLoading(true);
+      const url = category
+        ? `quotes.json?orderBy="category"&equalTo="${category}"`
+        : 'quotes.json';
+
+      const response: { data: IQuoteAPI } = await axiosAPI<IQuoteAPI>(url);
       if (response.data) {
-          const quoteFromAPI = Object.keys(response.data).map((quoteKey) => {
-            return {
-              ...response.data[quoteKey],
-              id: quoteKey,
-            };
-          });
+        const quoteFromAPI = Object.keys(response.data).map((quoteKey) => {
+          return {
+            ...response.data[quoteKey],
+            id: quoteKey,
+          };
+        });
         setQuotes(quoteFromAPI);
       }
-
     } catch (e) {
       console.error(e);
+    } finally {
+      setLoading(false);
     }
-  },[]);
+  }, [category]);
+
+
+  const deleteQuote = async (id: string) => {
+    try {
+      setLoading(true);
+      await axiosAPI.delete(`quotes/${id}.json`);
+      setQuotes((prevState) => prevState.filter((quote) => quote.id !== id));
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
 
   useEffect(() => {
     void fetchQuotes();
@@ -31,19 +57,29 @@ const Home = () => {
 
   return (
     <>
-      <>
-        {quotes.length === 0 ? (
-          <h2>No quotes</h2>
-        ) : (
-          <>
-            <Grid container spacing={2}>
-              {quotes.map((quote) => (
-                <QuoteItem key={quote.id} quote={quote} />
-              ))}
-            </Grid>
-          </>
-        )}
-      </>
+      {loading ? (
+          <Spinner/>
+        ) :
+        <div style={{display: 'flex', gap: '30px', marginBottom: '30px'}}>
+          <div style={{width: '170px'}}>
+            <Sidebar/>
+          </div>
+          {quotes.length === 0 ? (
+            <h2 style={{margin: '0'}}>No quotes</h2>
+          ) : (
+            <div style={{width: '100%'}}>
+              <h2
+                style={{margin: '0 0 20px 0'}}>{category ? category.charAt(0).toUpperCase() + category.slice(1) : 'All'}
+              </h2>
+              <Grid container spacing={2}>
+                {quotes.map((quote) => (
+                  <QuoteItem key={quote.id} quote={quote} deleteQuote={() => deleteQuote(quote.id)}/>
+                ))}
+              </Grid>
+            </div>
+          )}
+        </div>
+      }
     </>
   );
 };
