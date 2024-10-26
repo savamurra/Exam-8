501cbdaa2d2c4d4beec015079ddf54c4388d7004
Sanzhar Beishekeev <franki-55@mail.ru>
2024-10-26 17:31:19 +0600

    Добавил кнопку удаления и кнопку редактирования

--- src/components/QuoteItem/QuoteItem.tsx
+++ src/components/QuoteItem/QuoteItem.tsx
@@ -1,27 +1,37 @@
 import { IQuote } from '../../types';
 import * as React from 'react';
-import { Card, CardActions, CardContent, Typography } from '@mui/material';
+import { Button, Card, CardContent, Typography } from '@mui/material';
+import { NavLink } from 'react-router-dom';
 
 interface Props {
   quote: IQuote;
+  deleteQuote: (id: string) => void;
 }
 
-const QuoteItem: React.FC<Props> = ({quote}) => {
+const QuoteItem: React.FC<Props> = ({quote, deleteQuote}) => {
   return (
     <>
       <Card sx={{ width: "100%" }}>
-        <CardContent>
-          <Typography gutterBottom sx={{ fontSize: 20 }}>
-            {quote.author}
-          </Typography>
-        </CardContent>
-        <CardActions
-          style={{ display: "flex", justifyContent: "space-between" }}
+        <CardContent
+          style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}
         >
-          <Typography gutterBottom sx={{ fontSize: 20 }}>
-            {quote.category}
-          </Typography>
-        </CardActions>
+          <div>
+            <Typography gutterBottom sx={{ fontSize: 20, maxWidth: '800px'}}>
+              {quote.content}
+            </Typography>
+            <Typography gutterBottom sx={{ fontSize: 16 }}>
+              <strong>Автор: {quote.author}</strong>
+            </Typography>
+          </div>
+          <div>
+            <Button size="small" onClick={() => deleteQuote(quote.id)} color="warning">
+              Delete
+            </Button>
+            <Button size="small" component={NavLink} to={`/quotes/${quote.id}/edit`}>
+              Edit
+            </Button>
+          </div>
+        </CardContent>
       </Card>
     </>
   );
