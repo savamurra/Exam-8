bdff076ad6fe00a6ee4ddcf604faa8d377e5618e
Sanzhar Beishekeev <franki-55@mail.ru>
2024-10-26 17:34:06 +0600

    Добавил глобальные интерфейсы

---
+++ src/types.d.ts
@@ -0,0 +1,16 @@
+export interface IQuoteForm  {
+  author: string;
+  content: string;
+  category: string;
+}
+
+export interface IQuote {
+  author: string;
+  content: string;
+  category: string;
+  id: string;
+}
+
+export interface IQuoteAPI {
+  [id: string]: IQuote;
+}
