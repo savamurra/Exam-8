9b0a231bcab65b98e39a2b16aec4a51bd322f6c0
Sanzhar Beishekeev <franki-55@mail.ru>
2024-10-26 17:33:46 +0600

    Написал машрутизацию

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import './App.css';
+import Navbar from './components/Navbar/Navbar.tsx';
+import { Container } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+import Home from './containers/Home/Home.tsx';
+import NewQuote from './containers/NewQuote/NewQuote.tsx';
+import EditQuote from './containers/EditQuote/EditQuote.tsx';
+
 
 const App = () => {
 
-
   return (
     <>
-
+      <header>
+        <Navbar/>
+      </header>
+      <Container>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path="/quotes/category/:category" element={<Home/>} />
+          <Route path='/add/new-quote' element={<NewQuote/>}/>
+          <Route path='/quotes/:idQuote/edit' element={<EditQuote/>}/>
+          <Route path='*' element={<h1>Not found</h1>}/>
+        </Routes>
+      </Container>
     </>
   );
 };
