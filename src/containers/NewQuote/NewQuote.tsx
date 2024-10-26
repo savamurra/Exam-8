61e71a954a6220432a56d23a13e7e9e20a3fe597
Sanzhar Beishekeev <franki-55@mail.ru>
2024-10-26 17:33:21 +0600

    Создал компонент Spinner и добавил в те компоненты где есть запрос

--- src/containers/NewQuote/NewQuote.tsx
+++ src/containers/NewQuote/NewQuote.tsx
@@ -1,18 +1,34 @@
 import QuotesForm from '../../components/QuotesForm/QuotesForm.tsx';
 import { IQuoteForm } from '../../types';
 import axiosAPI from '../../axiosAPI.tsx';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import Spinner from '../../components/UI/Spinner/Spinner.tsx';
 
 const NewQuote = () => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState<boolean>(false);
 
   const submitForm = async (quote: IQuoteForm) => {
-    await axiosAPI.post("quotes.json", {
-      ...quote,
-    });
+    try {
+      await axiosAPI.post("quotes.json", {
+        ...quote,
+      });
+      navigate('/');
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+
   };
 
   return (
     <>
-     <QuotesForm submitForm={submitForm} />
+      {loading ? (
+        <Spinner/>
+      ) : <QuotesForm submitForm={submitForm} />
+      }
     </>
   );
 };
